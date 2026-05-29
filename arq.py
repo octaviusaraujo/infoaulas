feff86906c63d12d214cb39f8ce1a8f8d43b1161
Otávio Araújo <otavio.araujo@escolar.ifrn.edu.br>
2026-05-29 10:48:36 -0300

    prova danillo

---
+++ arq.py
@@ -0,0 +1,7 @@
+pontos_usuario = "150"
+x = int(pontos_usuario)
+
+bonus_adicional = "50"
+bonus_final = x + int(bonus_adicional)
+
+print("Pontuacao total calculada",bonus_final)
---
+++ arq1.py
@@ -0,0 +1,7 @@
+taxa_str = "94,7"
+
+taxa_str = float(taxa_str)
+
+taxa_cauculada = float(taxa_str)
+
+print("taxa de eficiencia otimizada", taxa_calculada, "%")
