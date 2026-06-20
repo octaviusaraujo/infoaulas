87a26fb3668c766014ab424b7978812fcd3b20b4
MIguel <migueldasensi@icloud.com>
2026-06-20 10:11:26 -0300

    logica

--- logica/Início em py/Atividades py/9carrinhodecompras.py
+++ logica/Início em py/Atividades py/9carrinhodecompras.py
@@ -4,15 +4,24 @@ compras = []
 
 while True:
     resposta = input("Deseja adicionar algo ao carrinho? (S/n) ")
-
     match resposta:
-        case "S":
+        case "S" | 's':
             produto = input("O que quer adicionar? ")
             compras.append(produto)
-
-        case "n":
+        case "n" | "N":
             break
+        case _:
+            print("Digite apenas S ou n.")
+print("Carrinho final:", compras)
 
+while True:
+    resposta = input("Deseja remover algo do carrinho? (S/n) ")
+    match resposta:
+        case "S" | "s":
+            produto = input("O que quer remover? ")
+            compras.remove(produto)
+        case "n" | "N":
+            break
         case _:
             print("Digite apenas S ou n.")
 
