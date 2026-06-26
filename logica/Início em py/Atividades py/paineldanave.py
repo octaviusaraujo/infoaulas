26af83595df734b4f90647895ed15e1fc84414ad
OTAVIO MIGUEL FERNANDES DE ARAUJO <20261181110031@ifrn.local>
2026-06-26 11:36:11 -0300

    logica

--- logica/Início em py/Atividades py/paineldanave.py
+++ logica/Início em py/Atividades py/paineldanave.py
@@ -1,22 +1,26 @@
-print("--Painel da nave--\n 1-Verificar combustivel\n 2-Ativar escudo de protecão \n 3- Ativar modo de auto destruicão")
-escolha = str(input("Digite uma opcão:"))
+print("--Painel da nave--\n 1-Verificar combustivel\n 2-Ativar escudo de protecão \n 3-\Ativar modo de auto destruicão")
+escolha = int(input("Digite uma opcão:"))
 while True:
     match escolha:
-     case "1":
-        combustivel = str(input("qual é a porcentagem do tanque?"))
+     case 1:
+        combustivel = int(input("qual é a porcentagem do tanque?"))
         if combustivel < 20:
             print("Baixo combustivel!!")
         else:
-            print("combustivel alto")
-     case "2":
-        mtr = input("Há meteoros por perto S/n?")
+            print("Nivel de combustivel seguro")
+            break
+     case 2:
+        mtr = str(input("Há meteoros por perto S ou n?")).lower()
         if mtr == 's':
             print("Escudo ativado")
-        else:
+        elif mtr == 'n':
             print("Ok, os escudos nao são necessarios!")
             break
-     case "3":
+        else:
+            print('Digite S/n')
+            break
+     case 3:
         print("MODO DE DESTRUICÃO ATIVADO, DESTRUICÃO EM 3,2,1...")
         break
      case "_":
-        print("")
+        print("digite 1,2 ou 3")
